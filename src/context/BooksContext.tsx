74acb344a0556735bba33a0f7ec9b160d0500505
kordomakto <hakimsanny898@gmail.com>
2026-09-22 11:22:52 +0300

    refactor: enhance BooksContext with TypeScript interfaces for better type safety and clarity

--- src/context/BooksContext.tsx
+++ src/context/BooksContext.tsx
@@ -1,21 +1,40 @@
-'use client'
-import React, { ReactNode, useState, createContext } from 'react';
+"use client";
+import { IBook } from "@/types/bookType";
+import React, {
+  ReactNode,
+  useState,
+  createContext,
+  Dispatch,
+  SetStateAction,
+} from "react";
 
-export const BooksContext = createContext<any>({})
+interface IBookContext {
+  readBooks: IBook[];
+  setReadBooks: Dispatch<SetStateAction<IBook[]>>;
+  wishlist: IBook[];
+  setWishlist: Dispatch<SetStateAction<IBook[]>>;
+}
 
-const BooksProvider = ({children}: {children: ReactNode}) => {
-    const [readBooks, setReadBooks]= useState([])
-    const [wishlist, setWishlist]= useState([])
+export const BooksContext = createContext<IBookContext>({
+  readBooks: [],
+  setReadBooks: () => {},
+  wishlist: [],
+  setWishlist: () => {},
+});
 
-    const sharedData = {
-        readBooks, 
-        setReadBooks, 
-        wishlist, 
-        setWishlist
-    }
-    return (
-        <BooksContext.Provider value ={sharedData}>{children}</BooksContext.Provider>
-    );
+const BooksProvider = ({ children }: { children: ReactNode }) => {
+  const [readBooks, setReadBooks] = useState<IBook[]>([]);
+  const [wishlist, setWishlist] = useState<IBook[]>([]);
+
+  const sharedData = {
+    readBooks,
+    setReadBooks,
+    wishlist,
+    setWishlist,
+  };
+  return (
+    <BooksContext.Provider value={sharedData}>{children}</BooksContext.Provider>
+  );
 };
 
 export default BooksProvider;
